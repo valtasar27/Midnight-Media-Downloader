3e1866aab5b323c7559351fefac1b65cd28f7182
ZSM <52359066+valtasar27@users.noreply.github.com>
2024-08-23 01:10:45 -0400

    Update functions.py
    Added quality choices, fixed bitrates to vars now.

--- Downloader/functions.py
+++ Downloader/functions.py
@@ -3,37 +3,65 @@ import yt_dlp as yt
 
 class Funtions_avg:
     
-    def download_mp4(self,url:str,file_name:str):
+    def download_mp4(self,url:str,file_name:str,quality:str):
+        match quality:
+            case "Fast (low quality)":
+                v_bitrate = '5M'
+                
+            case "Medium (default)":
+                v_bitrate = '10M'
+                
+            case "Slow (best quality)":
+                v_bitrate = '15M'
         
         try:
             input_file = url
             output_file = file_name + ".mp4"
             stream = fmp.input(input_file)
-            download = fmp.output(stream,output_file,format='mp4',video_bitrate='15M')
-            fmp.get_args(download)
+            download = fmp.output(stream,output_file,format='mp4',video_bitrate=v_bitrate)
             fmp.run_async(download)
         except Exception as e:
             print(e)
             
         
-    def download_mp3(self,url:str,file_name:str):
+    def download_mp3(self,url:str,file_name:str,quality:str):
+        
+        match quality:
+            case "Fast (low quality)":
+                a_bitrate = '96k'
+                
+            case "Medium (default)":
+                a_bitrate = '128k'
+                
+            case "Slow (best quality)":
+                a_bitrate = '196k'
         
         try:
             input_file = url
             output_file = file_name + ".mp3"
             stream = fmp.input(input_file)
-            download = fmp.output(stream,output_file,format='mp3')
+            download = fmp.output(stream,output_file,format='mp3',audio_bitrate=a_bitrate)
             fmp.run_async(download)
         except Exception as e:
             print(e)
         
-    def download_wav(self,url:str,file_name:str):
+    def download_wav(self,url:str,file_name:str,quality:str):
+        
+        match quality:
+            case "Fast (low quality)":
+                a_bitrate = '96k'
+                
+            case "Medium (default)":
+                a_bitrate = '128k'
+                
+            case "Slow (best quality)":
+                a_bitrate = '196k'
         
         try:
             input_file = url
             output_file = file_name + ".wav"
             stream = fmp.input(input_file)
-            download = fmp.output(stream,output_file,format='wav')
+            download = fmp.output(stream,output_file,format='wav',audio_bitrate=a_bitrate)
             fmp.run_async(download)
         except Exception as e:
             print(e)
